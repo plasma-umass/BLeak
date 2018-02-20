db9fe064992c45e590848acaf321ad20bd451d12
John Vilk <jvilk@cs.umass.edu>
2018-02-20 16:09:18 -0500

    Cleaning up rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@ import buble from 'rollup-plugin-buble';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-// import builtins from 'rollup-plugin-node-builtins';
-// import globals from 'rollup-plugin-node-globals';
 import {join} from 'path';
 
 const inBase = join(__dirname, 'build', 'browser', 'src');
@@ -33,27 +31,13 @@ export default {
       }
     }),
     resolve({
-      // use "module" field for ES6 module if possible
-      module: true, // Default: true
-
-      // use "jsnext:main" if possible
-      // – see https://github.com/rollup/rollup/wiki/jsnext:main
-      jsnext: true,  // Default: false
-
-      // use "main" field or index.js, even if it's not an ES6 module
-      // (needs to be converted from CommonJS to ES6
-      // – see https://github.com/rollup/rollup-plugin-commonjs
-      main: true,  // Default: true
-
-      // some package.json files have a `browser` field which
-      // specifies alternative files to load for people bundling
-      // for the browser. If that's you, use this option, otherwise
-      // pkg.browser will be ignored
-      browser: true  // Default: false
+      module: true,
+      jsnext: true,
+      main: true,
+      browser: true
     }),
     commonjs({
       namedExports: {
-        //'vis': ['Network', 'DataSet'],
         'react-dom': ['render'],
         'react': ['Component', 'createElement'],
         'brace': ['acequire']
@@ -63,7 +47,5 @@ export default {
       // Production for production builds.
       'process.env.NODE_ENV': JSON.stringify( 'development' )
     })
-    // builtins(),
-    // globals()
   ]
 };
